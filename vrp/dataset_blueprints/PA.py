3a418b7c3932d2349645c7da16524b262c80aa99
Camila Stenico <cstenico@shire.local>
2023-11-21 10:42:47 -0300

    new instance PA

--- vrp/dataset_blueprints/PA.py
+++ vrp/dataset_blueprints/PA.py
@@ -1,5 +1,8 @@
 from vrp.data_utils import InstanceBlueprint
 
 dataset = {}
-dataset['1'] = InstanceBlueprint(nb_customers=346, depot_position='C', customer_position='R', nb_customer_cluster=None,
+dataset['0'] = InstanceBlueprint(nb_customers=346, depot_position='C', customer_position='R', nb_customer_cluster=None,
+    demand_type='inter', demand_min=1, demand_max=10, capacity=180, grid_size=1000)  # PA.py
+
+dataset['1'] = InstanceBlueprint(nb_customers=99, depot_position='C', customer_position='R', nb_customer_cluster=None,
     demand_type='inter', demand_min=1, demand_max=10, capacity=180, grid_size=1000)  # PA.py
